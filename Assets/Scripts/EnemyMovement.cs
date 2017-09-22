3a931c41d4a2ee390f847ebcf198de88da6c93cc
Dreadfang <jannekarhu2@kao.fi>
2017-09-22 23:47:05 +0300

    :heart: :heart: :broken_heart:

---
+++ Assets/Scripts/EnemyMovement.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour {
+
+	public GameObject player;
+	Vector3 playerPos;
+	public float speed = 10;
+
+	void Start () 
+	{
+		
+	}
+	void Update ()
+	{
+		playerPos = player.transform.position;
+	}
+
+	void FixedUpdate () 
+	{
+		
+
+
+		transform.position = Vector2.MoveTowards(transform.position, playerPos, speed * Time.deltaTime);
+
+	}
+}
---
+++ Assets/Scripts/playerHealth.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class playerHealth : MonoBehaviour {
+	public Image healthBar;
+	public float maxHealth = 3;
+	public float health = 3;
+	float minhealth = 0;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		float calcHealth = health / maxHealth;
+		SetHealth (calcHealth);
+
+		if (health <= minhealth)
+		{
+			Debug.Log("You Died");
+		}
+	}
+	void SetHealth(float health)
+	{
+		healthBar.fillAmount = health;
+	}
+
+}
