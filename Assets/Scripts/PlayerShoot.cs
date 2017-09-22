eff7469fe627a8f1df466293d35b81e942b3d96d
Dreadfang <jannekarhu2@kao.fi>
2017-09-22 21:38:20 +0300

    Playershoot base + prefab

--- Assets/Scripts/PlayerShoot.cs
+++ Assets/Scripts/PlayerShoot.cs
@@ -4,13 +4,21 @@ using UnityEngine;
 
 public class PlayerShoot : MonoBehaviour {
 
-	// Use this for initialization
+	Vector3 mouse_pos;
+	Transform target; //Assign to the object you want to rotate
+	Vector3 object_pos;
+	float angle;
+
 	void Start () {
 		
 	}
 	
 	// Update is called once per frame
-	void Update () {
-		
+	void Update () 
+	{
+		var mouse = Input.mousePosition;
+		var screenPoint = Camera.main.WorldToScreenPoint(transform.localPosition);
+		var offset = new Vector2(mouse.x - screenPoint.x, mouse.y - screenPoint.y);
+		var angle = Mathf.Atan2(offset.y, offset.x) * Mathf.Rad2Deg;
 	}
 }
