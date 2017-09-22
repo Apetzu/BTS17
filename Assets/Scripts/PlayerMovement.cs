3073836b4538a3384c52e5db55bbaebb56d14d6b
Dreadfang <jannekarhu2@kao.fi>
2017-09-22 21:20:45 +0300

    Folders & stuff

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+
+	public float speed = 10;
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void FixedUpdate () {
+		float mx = Input.GetAxis("Horizontal");
+		float my = Input.GetAxis("Vertical");
+
+		Vector3 movement = new Vector3(mx, my, 0.0f);
+		transform.Translate(movement * speed * Time.deltaTime);
+
+	}
+}
