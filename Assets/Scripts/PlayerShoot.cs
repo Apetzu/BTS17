f612c86eccd7f80eea30750d627cae5a6bb31e5b
Aapo Ritvanen <aapo.ritvanen@gmail.com>
2017-09-23 13:23:41 +0300

    Prefab updates and more scripting

--- Assets/Scripts/PlayerShoot.cs
+++ Assets/Scripts/PlayerShoot.cs
@@ -6,11 +6,13 @@ public class PlayerShoot : MonoBehaviour {
 	float angle;
     Vector3 mousePos;
     Vector3 screenPoint;
+    public BoxCollider2D boxCol;
+    RaycastHit2D[] hitArray;
 
     void Start ()
     {
-		
-	}
+
+    }
 	
 	void Update () 
 	{
@@ -18,11 +20,18 @@ public class PlayerShoot : MonoBehaviour {
         screenPoint = Camera.main.WorldToScreenPoint(transform.localPosition);
         angle = Mathf.Atan2(mousePos.y - screenPoint.y, mousePos.x - screenPoint.x) * Mathf.Rad2Deg;
 
-        if (angle < 0)
+        /*if (angle < 0)
         {
             angle += 360;
+        }*/
+
+        boxCol.Raycast(new Vector2(mousePos.y - screenPoint.y, mousePos.x - screenPoint.x), hitArray);
+
+        for (int i = 0; i < hitArray.Length; i++)
+        {
+            Debug.Log(hitArray[i]);
         }
 
-        Debug.Log(angle);
+        //Debug.Log(angle);
 	}
 }
