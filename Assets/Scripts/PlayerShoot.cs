a00dbf843cce3df99d26608f3d8632e4c28eb66a
Aapo Ritvanen <aapo.ritvanen@gmail.com>
2017-09-23 14:09:17 +0300

    Player prefab tagged + more code to PlayerShoot

--- Assets/Scripts/PlayerShoot.cs
+++ Assets/Scripts/PlayerShoot.cs
@@ -3,11 +3,11 @@ using System.Collections;
 
 public class PlayerShoot : MonoBehaviour {
 
-	float angle;
     Vector3 mousePos;
     Vector3 screenPoint;
     public BoxCollider2D boxCol;
     RaycastHit2D[] hitArray;
+    Vector2 vectorDir;
 
     void Start ()
     {
@@ -18,20 +18,20 @@ public class PlayerShoot : MonoBehaviour {
 	{
 		mousePos = Input.mousePosition;
         screenPoint = Camera.main.WorldToScreenPoint(transform.localPosition);
-        angle = Mathf.Atan2(mousePos.y - screenPoint.y, mousePos.x - screenPoint.x) * Mathf.Rad2Deg;
 
-        /*if (angle < 0)
-        {
-            angle += 360;
-        }*/
+        vectorDir = new Vector2(-(mousePos.y - screenPoint.y), -(mousePos.x - screenPoint.x));
+
+        /*hitArray = new RaycastHit2D[2];
 
         boxCol.Raycast(new Vector2(mousePos.y - screenPoint.y, mousePos.x - screenPoint.x), hitArray);
 
         for (int i = 0; i < hitArray.Length; i++)
         {
-            Debug.Log(hitArray[i]);
-        }
+            Debug.Log(i + " " + hitArray[i].collider);
+        }*/
 
-        //Debug.Log(angle);
-	}
+        Debug.DrawRay(transform.position, vectorDir);
+
+        Debug.Log(vectorDir);
+    }
 }
