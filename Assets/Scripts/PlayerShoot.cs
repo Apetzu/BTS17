02c2246256974491e2d77c12d18e39b683ae6217
Dreadfang <jannekarhu2@kao.fi>
2017-09-24 15:54:39 +0300

    animator works

--- Assets/Scripts/PlayerShoot.cs
+++ Assets/Scripts/PlayerShoot.cs
@@ -32,42 +32,58 @@ public class PlayerShoot : MonoBehaviour {
 
         if (angle >= -(360/8/2) && angle <= 360/8/2)
         {
-            // 1 flip
+            // 1 flip 2
+			GetComponent<SpriteRenderer>().flipX = true;
+			GetComponent<Animator> ().SetInteger ("direction",2);
             Debug.Log("vittu 1");
         }
         else if (angle > 360 / 8 / 2 && angle < (360 / 8 / 2) + 360 / 8)
         {
-            // 2 flip
+            // 2 flip 3
+			GetComponent<SpriteRenderer>().flipX = true;
+			GetComponent<Animator> ().SetInteger ("direction",3);
             Debug.Log("vittu 2");
         }
         else if (angle > (360 / 8 / 2) + 360 / 8 && angle < (360 / 8 / 2) + 360 / 8 * 2)
         {
             // 3
+			GetComponent<SpriteRenderer>().flipX = false;
+			GetComponent<Animator> ().SetInteger ("direction",1);
             Debug.Log("vittu 3");
         }
         else if (angle > (360 / 8 / 2) + 360 / 8 * 2 && angle < (360 / 8 / 2) + 360 / 8 * 3)
         {
             // 4
+			GetComponent<SpriteRenderer>().flipX = false;
+			GetComponent<Animator> ().SetInteger ("direction",3);
             Debug.Log("vittu 4");
         }
         else if (angle > (360 / 8 / 2) + 360 / 8 * 3 && angle < (360 / 8 / 2) + 360 / 8 * 4)
         {
             // 5
+			GetComponent<SpriteRenderer>().flipX = false;
+			GetComponent<Animator> ().SetInteger ("direction",2);
             Debug.Log("vittu 5");
         }
         else if (angle > (360 / 8 / 2) + 360 / 8 * 4 && angle < (360 / 8 / 2) + 360 / 8 * 5)
         {
             // 6
+			GetComponent<SpriteRenderer>().flipX = false;
+			GetComponent<Animator> ().SetInteger ("direction",4);
             Debug.Log("vittu 6");
         }
         else if (angle > (360 / 8 / 2) + 360 / 8 * 5 && angle < (360 / 8 / 2) + 360 / 8 * 6)
         {
             // 7
+			GetComponent<SpriteRenderer>().flipX = false;
+			GetComponent<Animator> ().SetInteger ("direction",0);
             Debug.Log("vittu 7");
         }
         else if (angle > (360 / 8 / 2) + 360 / 8 * 6 && angle < (360 / 8 / 2) + 360 / 8 * 7)
         {
             // 8 flip
+			GetComponent<SpriteRenderer>().flipX = true;
+			GetComponent<Animator> ().SetInteger ("direction",4);
             Debug.Log("vittu 8");
         }
 
